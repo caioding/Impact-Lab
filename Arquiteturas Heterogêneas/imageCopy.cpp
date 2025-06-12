ab0b12a0b147357390df5032639bd482b5769492
caioding <7caioc@gmail.com>
2025-06-11 20:45:00 -0400

    ✨ feat: new code for copy image into a new file

---
+++ Arquiteturas Heterogêneas/imageCopy.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ofstream image_Write;
+    ifstream image_Read;
+
+    image_Read.open("./images/image.ppm");
+    image_Write.open("./images/Newimage.ppm");
+
+    string Type;
+    string Width;
+    string Height;
+    string MaxColorValue;
+    string pixelR, pixelG, pixelB;
+
+    if (image_Read.is_open()) {
+
+        image_Read >> Type;
+        image_Read >> Width;
+        image_Read >> Height;
+        image_Read >> MaxColorValue;
+        
+        image_Write << Type << endl;
+        image_Write << Width << " " << Height << endl;
+        image_Write << MaxColorValue << endl;
+
+        // Read and write pixel data with for loop
+        for (int ImgH = 0; ImgH < stoi(Height); ImgH++) {
+            for (int ImgV = 0; ImgV < stoi(Width); ImgV++) {
+                image_Read >> pixelR >> pixelG >> pixelB;
+                image_Write << pixelR << " " << pixelG << " " << pixelB << " ";
+            }
+            image_Write << endl;
+        }
+    }
+
+    image_Write.close();
+    image_Read.close();
+
+    return 0;
+}
